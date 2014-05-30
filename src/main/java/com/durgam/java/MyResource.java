b81eaec3669eb155b872969015f4cd664bb9bd74
Praveen Durgam <praveen.durgam@gmail.com>
2014-05-29 22:37:07 -0400

    initial commit

---
+++ src/main/java/com/durgam/java/MyResource.java
@@ -0,0 +1,27 @@
+
+package com.durgam.java;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import org.slf4j.*;
+
+/** Example resource class hosted at the URI path "/myresource"
+ */
+@Path("/myresource")
+@Stateless
+public class MyResource {
+    
+	private static final Logger log = LoggerFactory.getLogger(MyResource.class);
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String getIt() {
+    	log.info("Test Logging Rest Resource");
+        return "Hi there! This is Praveen";
+    }
+}
